57e29f30817f96e019e3eebdb693df07f953dc6d
vinhht <ho.tuong.vinh@gmail.com>
2023-11-29 08:44:16 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide")
 
 
 # Create a Streamlit app title and description
-st.title("SDA: Survey Data Analysis Easy")
+st.title("SDA: Survey Data Analysis")
 st.header("With Regression and SEM")
 st.write("This app helps you analyze survey data using Regression and SEM models.")
 st.write('\n\n-----------\n\n\n\n')
